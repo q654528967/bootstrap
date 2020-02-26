2f650c23af216d722b438a49c857f01971d8d361
q654528967 <654528967@qq.com>
2020-02-26 10:42:37 +0800

    删除bootstrap

--- bootstrap/js/iframeHeight.js
+++
@@ -1,7 +0,0 @@
-function reinitIframe(){
-  var iframe = document.getElementsByTagName("iframe")[0];
-  var bHeight = iframe.contentWindow.document.body.scrollHeight;
-  iframe.height = bHeight;
-  }
-  window.onload=reinitIframe;
-  window.addEventListener("resize",reinitIframe);
